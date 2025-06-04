51b5430e911743f567ecb7adfc78e012e5342c23
Maryam Fadaee <maryamfadaee@gmail.com>
2025-06-04 13:33:24 -0400

    create left sidebar in chat page

---
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./LeftSidebar.css";
+import { PiChatsBold } from "react-icons/pi";
+import { TiThMenuOutline } from "react-icons/ti";
+import { FaSearch } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa";
+
+const LeftSidebar = () => {
+	return (
+		<div className="left-sidebar">
+			<div className="left-sidebar-top">
+				<div className="left-sidebar-nav">
+					<div className="logo">
+						<PiChatsBold size={130} />
+
+						<p>Chat App</p>
+					</div>
+					<div className="menu">
+						<TiThMenuOutline size={16} />
+					</div>
+				</div>
+				<div className="left-sidebar-search">
+					<FaSearch size={16} />
+					<input type="text" placeholder="Search here ..." />
+				</div>
+			</div>
+			<div className="left-sidebar-list">
+				{Array(12)
+					.fill("")
+					.map((item, index) => (
+						<div key={index} className="friends">
+							<img src="/profile-pic-1.avif" alt="profile pic" />
+							<div>
+								<p>Richard Sanford</p>
+								<span>Hello, How are you?</span>
+							</div>
+						</div>
+					))}
+			</div>
+		</div>
+	);
+};
+
+export default LeftSidebar;
