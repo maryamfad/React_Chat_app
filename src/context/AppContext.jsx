7498fc668df1b246f5365875f2a845817c6a709e
Maryam Fadaee <maryamfadaee@gmail.com>
2025-06-18 18:00:04 -0400

    add functionality to get real chat data from db

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,5 +1,5 @@
-import { doc, getDoc, updateDoc } from "firebase/firestore";
-import { createContext, useState } from "react";
+import { doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
+import { createContext, useEffect, useState } from "react";
 import { auth, db } from "../config/firebase";
 import { toast } from "react-toastify";
 
@@ -35,6 +35,26 @@ const AppContextProvider = (props) => {
 		}
 	};
 
+	useEffect(() => {
+		if (userData) {
+			const chatRef = doc(db, "chat", userData.id);
+			const unsub = onSnapshot(chatRef, async (res) => {
+				const chatItems = res.data().chatData;
+				const tempData = [];
+				for (const item of chatItems) {
+					const userRef = doc(db, "users", item.rId);
+					const userSnap = await getDoc(userRef);
+					const userData = userSnap.data();
+					tempData.push({ ...item, userData });
+				}
+				setChatData(tempData.sort((a, b) => b.updatedAt - a.updatedAt));
+			});
+
+			return () => {
+				unsub();
+			};
+		}
+	}, [userData]);
 	const value = {
 		userData,
 		setUserData,
